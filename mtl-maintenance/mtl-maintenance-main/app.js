bbdf6629b9d660436e19c1e1af254c8a1899f804
MTL Maintenance <48396720+MTLMaintenance@users.noreply.github.com>
2026-07-24 11:28:33 -0600

    Remove bookmarkNextPage from global functions
    Removed bookmarkNextPage function from global scope.

--- mtl-maintenance/mtl-maintenance-main/app.js
+++ mtl-maintenance/mtl-maintenance-main/app.js
@@ -354,7 +354,6 @@ window.openModal = openModal;
 window.closeModal = closeModal;
 window.showPanel = showPanel;
 window.deleteDoc = deleteDoc;
-window.bookmarkNextPage = bookmarkNextPage;
 window.bookmarkPrevPage = bookmarkPrevPage;
 window.bookmarkCurrentPage = bookmarkCurrentPage;
 window.saveBookmarkRange = saveBookmarkRange;
