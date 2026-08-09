e5b5f8a5edfcd841069949f4f09e71b648d680e3
MTL Maintenance <48396720+MTLMaintenance@users.noreply.github.com>
2026-08-09 10:44:08 -0600

    Add customSymptoms to window state

--- mtl-maintenance/mtl-maintenance-main/app.js
+++ mtl-maintenance/mtl-maintenance-main/app.js
@@ -74,6 +74,7 @@ window.showRegister = () => {
     document.getElementById('register-view').style.display = 'grid';
     document.getElementById('auth-sub').textContent = 'Request access to MTL Maintenance';
 };
+window.state.customSymptoms
 window.showPending = showPending;
 window.doLogin = doLogin;
 window.doRegister = doRegister;
